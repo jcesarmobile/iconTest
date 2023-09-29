e0c06c9d7f2cabc8697f08482f9dbea79d8c0857
jcesarmobile <jcesarmobile@gmail.com>
2023-09-29 13:08:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'iconTest',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
